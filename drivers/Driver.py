fd2d7fcc5bcee3232caad85894ce536afa73d506
Bartosz Durys <bartek.durys@gmail.com>
2023-06-12 22:51:12 +0200

    Prepare base Driver class

--- drivers/Driver.py
+++ drivers/Driver.py
@@ -1,5 +1,29 @@
 # base class for the drivers inherit it to do own driver
+from typing import List
+from simulation.utils.Direction import Direction
+
+
 class Driver:
     def __init__(self):
+        '''direction_decisions: stores direction decisions of type simulation.utils.Direction
+
+        When a vehicle is ending road (entering crossroad) it will decide
+        to turn left, right, back or keep straight. If a list is empty or
+        the direction does not exist, it will be chosen random. While choosing
+        it will also pop the first element of a list.'''
+        self.direction_decisions:List[Direction] = []
+    
+    def on_road_start(self):
+        '''First tick after switching to a new road'''
         pass
-    # methods to discuss
+
+    def on_tick():
+        '''Runs on every change of map'''
+        pass
+
+    def on_road_end(self):
+        '''Last tick before choosing the next road'''
+        pass
+
+    def get_direction_decisions(self) -> Direction:
+        return self.direction_decisions and self.direction_decisions.pop(0)
--- simulation/utils/Vehicle.py
+++ simulation/utils/Vehicle.py
@@ -1,3 +1,5 @@
+from .Direction import Direction
+
 class Vehicle:
     from drivers import Driver
     def __init__(self, _id: int, driver: Driver):
@@ -6,3 +8,6 @@ class Vehicle:
         self.y = None
         self.driver = driver
         self.current_crossroad = None 
+    
+    def get_direction_decision(self) -> Direction:
+        return self.driver.get_direction_decisions()
