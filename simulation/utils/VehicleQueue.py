9f67d3e0e569b72b130dcea781285ad8f42bb1db
Bartosz Durys <bartek.durys@gmail.com>
2023-06-16 11:29:45 +0200

    Don't enter a road if busy. Wait one tick to run after next car

--- simulation/utils/VehicleQueue.py
+++ simulation/utils/VehicleQueue.py
@@ -12,15 +12,18 @@ class VehicleQueue:
         self.progress_step = cfg.VEHICLE_PROGRESS_STEP / self.road_length
         self.padding = padding
         self.is_green_callback = is_green_callback
+    
+    def is_busy(self):
+        return self.queue and self.queue[-1].progress == 0
 
     def enqueue(self, vehicle: Vehicle):
         vehicle.x, vehicle.y = self.start_pos
         self.queue.append(VehicleQueueElement(vehicle, progress=0))
         vehicle.on_road_start()
 
-    def move_closer(self):
+    def move_closer(self) -> List[VehicleQueueElement]:
         last_progress = None
-        finished_vehicles = []
+        finished_vqes = []
         for vqe in self.queue:
             vqe.vehicle.on_tick()
             if vqe.progress == 1 and not self.is_green_callback():
@@ -29,8 +32,8 @@ class VehicleQueue:
             if last_progress and vqe.progress + (self.padding/self.road_length) + self.progress_step >= last_progress:
                 last_progress = vqe.progress
                 continue
-            vqe.progress = min(vqe.progress + self.progress_step, 1)
             last_progress = vqe.progress
+            vqe.progress = min(vqe.progress + self.progress_step, 1)
 
             new_x = last_progress*(self.end_pos[0] - self.start_pos[0]) + self.start_pos[0]
             new_y = last_progress*(self.end_pos[1] - self.start_pos[1]) + self.start_pos[1]
@@ -38,6 +41,5 @@ class VehicleQueue:
 
             if vqe.progress == 1 and self.is_green_callback():
                 vqe.vehicle.on_road_end()
-                finished_vehicles.append(vqe.vehicle)
-                self.queue.remove(vqe)
-        return finished_vehicles
+                finished_vqes.append(vqe)
+        return finished_vqes
