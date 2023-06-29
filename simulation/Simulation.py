a81c1a7f82de83a11697b06cf8ffcb3510d683f0
Bartosz Durys <30702198+Bart63@users.noreply.github.com>
2023-06-29 14:31:40 +0200

    29 seryjne testowanie (#36)
    * Add yaml configuration for testing purposes
    
    * Running single test using command line arguments
    
    * Add series testing
    
    * Make logs prettier
    
    ---------
    
    Co-authored-by: pioter00 <228832@edu.p.lodz.pl>

--- simulation/Simulation.py
+++ simulation/Simulation.py
@@ -9,31 +9,39 @@ from .api import API
 import config as cfg
 import cv2
 import time
+from .utils.YamlConfigParser import set_yaml_config
 
 DESTINATION_REACH_DISTANCE = 1
 
+
 class Simulation:
-    def __init__(self, argv):
-        self.map = Map(size=(cfg.WIDTH, cfg.HEIGHT), seed=cfg.SEED, vehicles_number=cfg.NB_DUMMY_VEHICLES, map_filling=cfg.MAP_FILLING, road_padding=cfg.ROAD_PADDING)
-        self.destinations:List[Destination] = self.map.destinations
+    def __init__(self, driver_name: str, test_no: int, test_type: str):
+        self.main_vehicle = None
+        if driver_name != "" and test_no != -1:
+            set_yaml_config('configurations.yaml', driver_name, test_no, test_type)
+        self.map = Map(size=(cfg.WIDTH, cfg.HEIGHT), seed=cfg.SEED, vehicles_number=cfg.NB_DUMMY_VEHICLES,
+                       map_filling=cfg.MAP_FILLING, road_padding=cfg.ROAD_PADDING)
+        self.destinations: List[Destination] = self.map.destinations
         self.map_renderer = MapRenderer(self.map)
         self.set_init_api_values()
+        self.simulation_running = False
 
     def run(self):
         print('Welcome in the Simulation!')
+        self.simulation_running = True
 
         self.map.add_main_vehicle(cfg.MAIN_VEHICLE_DRIVER)
-        self.main_vehicle:Vehicle = self.map.main_vehicle
+        self.main_vehicle: Vehicle = self.map.main_vehicle
         self.set_final_dest_api_value()
 
         start_time = time.time()
         ticks = 0
-        while True:
+        while self.simulation_running:
             if ticks % cfg.TRAFFIC_LIGHTS_CHANGE_TICKS == 0:
                 self.map.switch_traffic_lights(cfg.TRAFFIC_LIGHTS_CHANGE_PERC)
 
             self.update_rest_api_values()
-            
+
             if ticks == 0:
                 self.on_simulation_start()
 
@@ -49,13 +57,21 @@ class Simulation:
                 break
         end_time = time.time()
         time_taken = end_time - start_time
-        print('Distance: ', self.main_vehicle.distance)
-        print('Time: ', time_taken)
+        # print('Distance: ', self.main_vehicle.distance)
+        # print('Time: ', time_taken)
         self.end_simulation()
-    
+        return {
+            'distance': self.main_vehicle.distance,
+            'time': time_taken,
+            'ticks': ticks
+        }
+
+    def stop(self):
+        self.simulation_running = False
+
     def try_reach_destination(self):
         vx, vy = self.main_vehicle.get_position()
-        
+
         destinations = self.destinations
         if len(destinations) > 1:
             destinations = list(filter(lambda d: not d.is_last, destinations))
@@ -68,24 +84,25 @@ class Simulation:
             if not mask:
                 continue
             self.map.destination_reach(destination)
-                
+
     def end_simulation(self):
         print('Simulation ended')
-        
+
     def on_simulation_start(self):
         for v in self.map.vehicles:
             v.on_simulation_start()
-            
+
     def set_init_api_values(self):
         crossroads = self.map.crossroads
         API.crossroads_pos_list = [(cr.x, cr.y) for cr in crossroads]
         API.possible_directions = [cr.get_connection_directions() for cr in crossroads]
-        API.roads_length_per_crossroad = [{dir:cr.get_road_length(dir) for dir in API.possible_directions[i]} for i, cr in enumerate(crossroads)]
+        API.roads_length_per_crossroad = [{dir: cr.get_road_length(dir) for dir in API.possible_directions[i]} for i, cr
+                                          in enumerate(crossroads)]
 
     def set_final_dest_api_value(self):
         final_destination = self.map.last_destination
         API.final_destination_pos = final_destination.x, final_destination.y
-    
+
     def update_rest_api_values(self):
         API.main_vehicle_pos = self.map.main_vehicle.get_position()
         API.main_vehicle_direction = self.map.main_vehicle.current_direction
@@ -93,4 +110,5 @@ class Simulation:
         API.target_crossroad_possible_directions = self.map.main_vehicle.target_crossroad.get_connection_directions()
         API.destinations_pos_list = [des.get_position() for des in self.destinations]
         crossroads = self.map.crossroads
-        API.cars_on_road = [{dir:len(cr.vehicle_queue[dir].queue) for dir in API.possible_directions[i]} for i, cr in enumerate(crossroads)]
+        API.cars_on_road = [{dir: len(cr.vehicle_queue[dir].queue) for dir in API.possible_directions[i]} for i, cr in
+                            enumerate(crossroads)]
